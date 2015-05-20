0b3f875be39630f2c4b0a45f57e72687b3b0abe0
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 20:45:59 +0100

    adding scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,6 +24,6 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, '1477418045830693', '229aedb0870e50581b6f62411382e6a7', scope: 'user,public_repo'
+   config.omniauth :facebook, '1477418045830693', '229aedb0870e50581b6f62411382e6a7', scope: 'public_profile,email,user_friends'
 
 end
