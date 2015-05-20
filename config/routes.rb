37cde57ea3501472db8cf47f16f48d77826d7c8b
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 20:59:11 +0100

    fixing callbacks 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :facebook_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :products
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
   root 'products#index'
 
 
