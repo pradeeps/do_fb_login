6ce85ab9398e627e619bdab4ab5b03d6b94f58dc
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 20:33:34 +0100

    another FB implementation

---
+++ config/initializers/devise.rb
@@ -0,0 +1,29 @@
+Devise.setup do |config|
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+
+  require 'devise/orm/active_record'
+
+  config.case_insensitive_keys = [ :email ]
+
+  config.strip_whitespace_keys = [ :email ]
+
+  config.skip_session_storage = [:http_auth]
+
+  config.stretches = Rails.env.test? ? 1 : 10
+
+  config.reconfirmable = true
+
+  config.expire_all_remember_me_on_sign_out = true
+
+  config.password_length = 8..128
+
+  config.reset_password_within = 6.hours
+
+  config.sign_out_via = :delete
+
+  # ==> OmniAuth
+  # Add a new OmniAuth provider. Check the wiki for more information on setting
+  # up on your models and hooks.
+   config.omniauth :facebook, '1477418045830693', '229aedb0870e50581b6f62411382e6a7', scope: 'user,public_repo'
+
+end
