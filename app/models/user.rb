1f8d39b1bcd23ae2d2c800ce42606d53e1e9de2a
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 20:53:01 +0100

    ading to_s

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(provider: auth.provider, uid: auth.uid.to_s).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
